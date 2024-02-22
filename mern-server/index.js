afa5f846c91ade1e789db7198f33dc4c17c6af16
Amantha Nadeeshan <amanthahettige@gmail.com>
2024-02-22 22:42:21 +0530

    connect the right database
    Fixed some issues and recreate the new database and connect it

--- mern-server/index.js
+++ mern-server/index.js
@@ -4,28 +4,63 @@ const port = process.env.port || 5000
 const mongoose = require('mongoose')
 const cors = require('cors')
 
-//middleware
+//middleware.........................................................................
 app.use(cors());
 app.use(express.json());
 
-//5SrNvNhZKbuKzUWL
+//XcOY4cfbUICgjrgt.......
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-//connect to Mongodb
+//Mongodb configurations
 
-const uri = 'mongodb://localhost:27017/';
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mern-book-store:XcOY4cfbUICgjrgt@atlascluster.9zpqbg6.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster";
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-    // Start your Express server once connected
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
-  })
-  .catch(err => console.error('Error connecting to MongoDB:', err));
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    //create a collection of documents
+    const bookcollections = client.db("BookInventory").collection("books");
+
+    //insert a book to the db:post method
+
+    app.post("/upload-book",async(req,res)=>{
+      const data = req.body;
+      const result = await bookcollections.insertOne(data);
+      res.send(result);
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app. listen(port, () => {
+   console.log('Example app listening on port ${port}')
+})
+
+  
 
 
